9bf8bcc7fb09d9bd89e07ba220f0e8e9f9a45d7e
Gabriele Girelli <gigi.ga90@gmail.com>
2019-03-27 17:13:40 +0100

    Implemented batched records joiner.

--- kman/join.py
+++ kman/join.py
@@ -6,6 +6,10 @@
 '''
 
 from enum import Enum
+from heapq import merge
+from kman.batch import Batch
+import time
+from tqdm import tqdm
 
 class KJoiner(object):
 	"""docstring for KJoiner"""
@@ -20,10 +24,28 @@ class KJoiner(object):
 
 	def __init__(self, mode = None):
 		super(KJoiner, self).__init__()
+		if type(mode) != type(None):
+			assert mode in self.MODES
+			self.__mode = mode
 
 	@property
 	def mode():
 		return self.__mode
 
-	def join(self, batches):
-		pass
+	def join(self, batches, outpath, doSort = False):
+
+		assert all([type(b) == Batch for b in batches])
+		if doSort:
+			generators = [((r.header, r.seq) for r in b.sorted)
+				for b in batches]
+		else:
+			generators = [((r.header, r.seq) for r in b.record_gen())
+				for b in batches]
+
+		nrecords = sum([b.current_size for b in batches])
+		crawler = merge(*generators, key = lambda x: x[1])
+		
+		with open(outpath, "w+") as OH:
+			for record in tqdm(crawler, total = nrecords):
+				time.sleep(0.01)
+				pass
