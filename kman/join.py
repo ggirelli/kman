afe3b20abe94fff89759967d511cd2d82163a530
Gabriele Girelli <gigi.ga90@gmail.com>
2019-03-28 09:21:35 +0100

    Added joinin modes.

--- kman/join.py
+++ kman/join.py
@@ -17,23 +17,32 @@ class KJoiner(object):
 	class MODES(Enum):
 		UNIQUE = 1
 		SEQ_COUNT = 2
-		VCOUNT = 3
+		VEC_COUNT = 3
 	DEFAULT_MODE = MODES.UNIQUE
 
 	__mode = DEFAULT_MODE
+	__join_function = None
 
 	def __init__(self, mode = None):
 		super(KJoiner, self).__init__()
 		if type(mode) != type(None):
 			assert mode in self.MODES
 			self.__mode = mode
+		if self.mode == self.MODES.UNIQUE:
+			self.__join_function = self.__join_unique
+		elif self.mode == self.MODES.SEQ_COUNT:
+			self.__join_function = self.__join_sequence_count
+		elif self.mode == self.MODES.VEC_COUNT:
+			self.__join_function = self.__join_vector_count
 
 	@property
-	def mode():
+	def mode(self):
 		return self.__mode
+	@property
+	def join_function(self):
+		return self.__join_function
 
 	def join(self, batches, outpath, doSort = False):
-
 		assert all([type(b) == Batch for b in batches])
 		if doSort:
 			generators = [((r.header, r.seq) for r in b.sorted)
@@ -44,8 +53,28 @@ class KJoiner(object):
 
 		nrecords = sum([b.current_size for b in batches])
 		crawler = merge(*generators, key = lambda x: x[1])
-		
+
 		with open(outpath, "w+") as OH:
+			first_record = next(crawler)
+			current_seq = first_record[1]
+			current_headers = [first_record[0]]
 			for record in tqdm(crawler, total = nrecords):
-				time.sleep(0.01)
-				pass
+				if current_seq == record[1]:
+					current_headers.append(record[0])
+				else:
+					self.join_function(OH, (current_seq, current_headers))
+
+	def __join_unique(self, OH, batch):
+		print(batch)
+		import sys; sys.exit()
+		pass
+
+	def __join_sequence_count(self, OH, batch):
+		print(batch)
+		import sys; sys.exit()
+		pass
+
+	def __join_vector_count(self, OH, batch):
+		print(batch)
+		import sys; sys.exit()
+		pass
