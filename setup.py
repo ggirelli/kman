1e1e01e683318d42616ef232f74f6cb0ed8c2d23
Gabriele Girelli <gigi.ga90@gmail.com>
2019-04-15 16:50:06 +0200

    Fixed broken link

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(name='kman',
 		"oligo_melting",
 		"tqdm"
 	],
-	scripts=["bin/kmer_batch", "bin/kmer_count", "bin/kmer_uniq", "bin/kmer_test"],
+	scripts=["bin/kmer_batch", "bin/kmer_count", "bin/kmer_uniq"],
 	test_suite="nose.collector",
 	tests_require=["nose"],
 )
