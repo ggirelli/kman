c4d2fe924506c255fe61a9313ad934b3fd57e251
Gabriele Girelli <gigi.ga90@gmail.com>
2021-03-23 17:35:04 +0100

    Updated version managing.

--- kman/__init__.py
+++ kman/__init__.py
@@ -7,7 +7,15 @@ from kman import asserts, const, io
 from kman import batch, batcher
 from kman import abundance, seq, join
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    pass
+
 __all__ = [
+    "__version__",
     "abundance",
     "asserts",
     "batch",
